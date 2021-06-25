dd0e78375676bbd1c92a2c464975817752d8e3a8
lapatka-py <kozlov.ds@phystech.edu>
2021-06-25 22:07:54 +0300

    popa

---
+++ models/sjdgskjg.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import copy
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision.models as models
+import torchvision.transforms as transforms
+from PIL import Image
+from scipy import misc
+
+content_layers_default = ["conv_4"]
+
+
+def gram_matrix(input):
+    batch_size, h, w, f_map_num = input.size()  # batch size(=1)
+    features = input.view(batch_size * h, w * f_map_num)  # resise F_XL into \hat F_XL
+
+    G = torch.mm(features, features.t())  # compute the gram product
+
+    # we 'normalize' the values of the gram matrix
+    # by dividing by the number of element in each feature maps.
+    return G.div(batch_size * h * w * f_map_num)
