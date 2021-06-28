07fc0018cea12555674c19f0927ea0215ea598af
daniilzakurnaev <71659236+daniilzakurnaev@users.noreply.github.com>
2021-06-28 12:24:08 +0300

    Update st_model.py

--- models/st_model.py
+++ models/st_model.py
@@ -61,3 +61,57 @@ class Normalization(nn.Module):
     def forward(self, img):
         # normalize img
         return (img - self.mean) / self.std
+
+class StyleTransferModel:
+    def run_style_transfer(self, cnn, normalization_mean, normalization_std,
+                           content_img, style_img, input_img, num_steps=500,
+                           style_weight=100000, content_weight=1):
+        """Run the style transfer."""
+        print('Building the style transfer model..')
+        model, style_losses, content_losses = self.get_style_model_and_losses(cnn,
+                                                                              normalization_mean, normalization_std,
+                                                                              style_img, content_img)
+        optimizer = self.get_input_optimizer(input_img)
+
+        print('Optimizing..')
+        run = [0]
+        while run[0] <= num_steps:
+
+            def closure():
+                # correct the values
+                # это для того, чтобы значения тензора картинки не выходили за пределы [0;1]
+                input_img.data.clamp_(0, 1)
+
+                optimizer.zero_grad()
+
+                model(input_img)
+
+                style_score = 0
+                content_score = 0
+
+                for sl in style_losses:
+                    style_score += sl.loss
+                for cl in content_losses:
+                    content_score += cl.loss
+
+                # взвешивание ощибки
+                style_score *= style_weight
+                content_score *= content_weight
+
+                loss = style_score + content_score
+                loss.backward()
+
+                run[0] += 1
+                if run[0] % 50 == 0:
+                    print("run {}:".format(run))
+                    print('Style Loss : {:4f} Content Loss: {:4f}'.format(
+                        style_score.item(), content_score.item()))
+                    print()
+
+                return style_score + content_score
+
+            optimizer.step(closure)
+
+        input_img.data.clamp_(0, 1)
+
+        return input_img
